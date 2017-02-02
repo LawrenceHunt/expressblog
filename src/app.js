ebc032b6ddfc004ef6ab7008eec58e7b165e7738
Lawrence Hunt <lawrence.hunt@hotmail.com>
2017-02-02 18:07:06 +0000

    rendering template for index and post

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 'use strict';
 var express = require('express');
+var posts = require('./mock/posts.json');
 
 var app = express();
 
-app.get('/', function (request, response) {
-  response.send("Helloooooo!");
-  app.done()
-})
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates');
 
-app.listen(3000);
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/blog/:title?', function(req, res) {
+  var title = req.params.title;
+  if(title === undefined) {
+    res.status(503);
+    res.send("this page is under construction");
+  } else {
+    var post = posts[title] || {};
+    res.render('post', { post: post });
+  }
+});
+
+app.listen(3000, function(){
+  console.log("The frontend server is running on port 3000!");
+});
